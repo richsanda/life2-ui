3df4685a99b845183885b13c1c513056ece3e08a
sg0307091 <rich.sanda@sabre.com>
2022-02-12 22:49:22 -0500

    get rid of old note functionality... it's just life2 now

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import React, { FunctionComponent, useState, useEffect, useContext } from "react";
 import "./styles/styles.css";
 import "./styles/neat.css";
-import { Route, Switch, useHistory } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import ArtifactContext from "./contexts/ArtifactContext";
 import NeatContext from "./contexts/NeatContext";
 import ErrorBoundary from "./ErrorBoundary";
-import ArtifactAPI from "./hooks/artifactApi";
-import Neat from "./pages/Neat";
 import Life2 from "./pages/Life2";
 
 const App: FunctionComponent<{}> = () => {
@@ -24,7 +22,6 @@ const App: FunctionComponent<{}> = () => {
         <ArtifactContext.Provider value={artifactContextState}>
           <Switch>
             <Route exact path="/life2" component={Life2} />
-            <Route exact path="/neat" component={Neat} />
           </Switch>
         </ArtifactContext.Provider>
       </NeatContext.Provider>
