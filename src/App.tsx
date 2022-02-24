0642e4a91f3b0438176e84ed2b97915efa9d9ce1
sg0307091 <rich.sanda@sabre.com>
2022-02-23 20:00:38 -0500

    remove old contexts

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,18 @@
-import React, { FunctionComponent, useState, useContext } from "react";
+import { FunctionComponent } from "react";
 import "./styles/styles.css";
 import "./styles/neat.css";
 import { Route, Switch } from "react-router-dom";
-import ArtifactContext from "./contexts/ArtifactContext";
 import ErrorBoundary from "./ErrorBoundary";
 import Life2 from "./pages/Life2";
 
 const App: FunctionComponent<{}> = () => {
 
-  const [artifactContext] = useContext(ArtifactContext);
-  const artifactContextState = useState(artifactContext);
-
   return (
 
     <ErrorBoundary>
-        <ArtifactContext.Provider value={artifactContextState}>
-          <Switch>
-            <Route exact path="/life2" component={Life2} />
-          </Switch>
-        </ArtifactContext.Provider>
+      <Switch>
+        <Route exact path="/life2" component={Life2} />
+      </Switch>
     </ErrorBoundary>
   );
 };
